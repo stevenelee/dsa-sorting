918b276e6ac08f9c97f47db6bc491b7b5068a457
Steven Lee <stevelee83@gmail.com>
2023-06-16 11:34:50 -0700

    finish exercise

--- bubble.js
+++ bubble.js
@@ -1,3 +1,14 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    console.log("after first loop", arr)
+    for (let j = 0; j <= i-1; j++) {
+      console.log("after second loop", arr)
+      if (arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,32 @@
-function merge() {}
+function merge(arr1, arr2) {
+  let mergedArr = [];
+  let arr1Pointer = 0;
+  let arr2Pointer = 0;
 
-function mergeSort() {}
+  while (arr1Pointer < arr1.length && arr2Pointer < arr2.length) {
+    if (arr1[arr1Pointer] < arr2[arr2Pointer]) {
+      mergedArr.push(arr1[arr1Pointer]);
+      arr1Pointer++;
+    } else {
+      mergedArr.push(arr2[arr2Pointer]);
+      arr2Pointer++;
+    }
+  }
+
+  if (arr1Pointer >= arr1.length) {
+    return mergedArr.concat(arr2.slice(arr2Pointer));
+  }
+
+  return mergedArr.concat(arr1.slice(arr1Pointer));
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let middleOfArr = Math.floor(arr.length / 2)
+  let arr1 = arr.slice(0, middleOfArr);
+  let arr2 = arr.slice(middleOfArr);
+
+  return merge(mergeSort(arr1), mergeSort(arr2));
+}
 
 module.exports = { merge, mergeSort};
